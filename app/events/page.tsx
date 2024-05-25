47f852651482257a3c79d5565fa0821a187a9293
Jing <ojing12321@gmail.com>
2024-05-25 15:17:42 +0800

    modified code

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,3 @@
 export default function Page() {
-  return (
-   <main>
-    hello world
-    </main>
-  );
-}
+    return <p>event Page</p>;
+  }
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      hello world
+    </main>
+  );
+}
