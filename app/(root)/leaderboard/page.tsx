794ff21f1431f42c3ce22c3609f0b268c21bb1eb
jack-thant <thanthtoo5728@gmail.com>
2024-05-26 03:19:48 +0800

    Fix UI on Leaderboard page

--- app/(root)/leaderboard/page.tsx
+++ app/(root)/leaderboard/page.tsx
@@ -10,39 +10,38 @@ export default async function Leaderboard() {
   users.sort((a, b) => b.xp - a.xp);
 
   return (
-    <div className="flex flex-col h-full w-full p-8 bg-gray-100">
-      <h2 className="mb-6 text-2xl font-bold text-gray-800 md:text-3xl">
-        OtterPals Leaderboard
-      </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-white shadow-lg p-6 overflow-auto">
-        <div className="rounded-lg p-6">
+    <div className="flex flex-col h-full w-full">
+      <h1 className='head-text mb-5'>OtterPals LeaderBoard</h1>
+      <div className="flex grow flex-col justify-between rounded-xl bg-dark-2 shadow-lg px-5 overflow-auto">
+        <div className="rounded-lg">
           {users.map((user, i) => (
             <div
               key={user.id}
               className={clsx(
                 'flex flex-row items-center justify-between py-4',
                 {
-                  'border-t border-gray-200': i !== 0,
+                  'border-t border-gray-700': i !== 0,
                 }
               )}
             >
               <div className="flex items-center">
-                <span className={clsx('mr-4 text-lg font-bold', {
+                <span className={clsx('mr-4 text-lg font-semibold', {
                   'text-yellow-500': i === 0, // First place
-                  'text-gray-700': i === 1, // Second place
-                  'text-yellow-600': i === 2, // Third place
+                  'text-stone-400': i === 1, // Second place
+                  'text-amber-600': i === 2, // Third place
+                  'text-light-2': i > 2
                 })}>
                   {i + 1}.
                 </span>
                 <Image
-                  src={user.image_url}
+                  src={user.userImage}
                   alt={`${user.name}'s profile picture`}
                   className="mr-4 rounded-full"
                   width={40}
                   height={40}
                 />
                 <div className="min-w-0">
-                  <p className="truncate text-sm font-semibold text-gray-800 md:text-base">
+                  <p className="truncate text-sm font-semibold text-primary-500 md:text-base">
                     {user.name}
                   </p>
                   <p className="hidden text-sm text-gray-500 sm:block">
@@ -50,13 +49,18 @@ export default async function Leaderboard() {
                   </p>
                 </div>
               </div>
-              <p className="truncate text-sm font-medium text-gray-700 md:text-base">
-                {user.xp} XP
-              </p>
+              <p className={clsx('mr-4 text-lg font-bold', {
+                  'text-yellow-500': i === 0, // First place
+                  'text-stone-400': i === 1, // Second place
+                  'text-amber-600': i === 2, // Third place
+                  'text-light-2': i > 2
+                })}>
+                  {user.xp} XP
+                </p>
             </div>
           ))}
         </div>
-        <div className="flex items-center justify-center pt-6">
+        <div className="flex items-center justify-center py-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
         </div>
